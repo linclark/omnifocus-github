8d4e6e3cdaef0fce241f3c54edd40f400e1745cf
Lin Clark <lin.w.clark@gmail.com>
2015-01-03 18:31:22 -0500

    update README and add example

---
+++ example.sh
@@ -0,0 +1,8 @@
+# 1. Download http://pixelsnatch.com/omnifocus/OmniFocus.scpt and place in ~/Library/Script Libraries
+# 2. Create an OAuth token on GitHub
+# 3. Create a file named ~/.omnifocus-github and fill in values:
+#
+#    token: <your GitHub auth token here>
+# 4. Then run:
+
+omnigraffle-github
